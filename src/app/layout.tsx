703a1a1167b5d977aeb403e3b25eb8993a63b2aa
JiwanChaudhary <jeewanchaudhary6@gmail.com>
2023-11-27 17:55:36 +0545

    footer completed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Header from "@/components/header";
 import ActiveSectionConextProvider from "@/context/active-section-context";
 import { Toaster } from "react-hot-toast";
+import Footer from "@/components/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -28,6 +29,7 @@ export default function RootLayout({
           <Header />
           {children}
           <Toaster position="top-right" />
+          <Footer />
         </ActiveSectionConextProvider>
       </body>
     </html>
---
+++ src/components/footer.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="mb-10 px-4 text-center text-gray-500">
+      <small className="mb-2 text-xs block">
+        &copy; 2023 Jiwan. All Rights Reserved.
+      </small>
+      <p className=" text-xs">
+        <span className=" font-semibold">About this website:</span>{" "}
+        built with React and Next.js(App Router and Server Actions), TypeScript,
+        Tailwind CSS, Framer Motion, React Email & Resend.
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
