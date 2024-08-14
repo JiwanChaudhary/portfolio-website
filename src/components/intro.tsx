90770d0f72a91d8bf7d39a082f866135784927cb
JiwanChaudhary <jeewanchaudhary6@gmail.com>
2024-08-14 23:54:41 +0545

    update resume

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -93,7 +93,7 @@ const Intro = () => {
 
         {/* resume */}
         <a
-          href="/resumeJiwan.pdf"
+          href="/JiwanChaudhary.pdf"
           download
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
         >
